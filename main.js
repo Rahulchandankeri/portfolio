f157d75cbf252e1e11f9d56a043b4bb9cb276498
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-03-25 19:08:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,17 @@ function myFunction(x) {
       // Get the modal
       var popup = document.getElementById("mypopup");
       var popup2 = document.getElementById("mypopup2");
+      var popup3 = document.getElementById("mypopup3");
       
       // Get the button that opens the modal
       var btn = document.getElementById("myBtn");
       var btn2 = document.getElementById("myBtn2");
+      var btn3 = document.getElementById("myBtn3");
       
       // Get the <span> element that closes the modal
       var span = document.getElementsByClassName("close")[0];
       var span2 = document.getElementsByClassName("close")[1];
+      var span3 = document.getElementsByClassName("close")[2];
       
       // When the user clicks the button, open the modal 
       btn.onclick = function() {
@@ -26,12 +29,18 @@ function myFunction(x) {
       btn2.onclick = function() {
         popup2.style.display = "block";
       }
+      btn3.onclick = function() {
+        popup3.style.display = "block";
+      }
       // When the user clicks on <span> (x), close the modal
       span.onclick = function() {
         popup.style.display = "none";
       }
       span2.onclick = function() {
         popup2.style.display = "none";
+      }
+      span3.onclick = function() {
+        popup3.style.display = "none";
       }
       
       // When the user clicks anywhere outside of the modal, close it
@@ -42,6 +51,9 @@ function myFunction(x) {
         if (event.target == popup2) {
             popup2.style.display = "none";
         }
+        if (event.target == popup3) {
+          popup3.style.display = "none";
+      }
       }
       
       
