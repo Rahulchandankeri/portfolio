882238c22e7385d48aadcdd0e324ca6b3594bd21
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-10-13 22:30:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const project = document.querySelectorAll(".project-container");
+
+// if (project.length > 3) {
+//   project.style.display = "none";
+// } else {
+//   console.log("Error");
+// }
+
+// document.querySelector(".loadmore").addEventListener("click", function () {
+//   const projectContainer = document.querySelectorAll(".project-container");
+//   console.log(projectContainer);
+//   for (let i = 0; i < projectContainer.length; i++) {
+//     console.log(projectContainer);
+//     if (projectContainer.length > 3) {
+//       projectContainer.style.display = "none";
+//     } else {
+//       console.log("Error");
+//     }
+//   }
+// });
